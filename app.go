ece89eb33a88e62735a0580f0cbaf0f0045038d1
Nitipat L <iamsvz@gmail.com>
2020-03-08 18:47:04 +0700

    [Edited] add custom cors

--- app.go
+++ app.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
 	"app/src/controllers"
 	"app/src/routes"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -27,6 +27,30 @@ func NewAppDatabase(userHandler controllers.UserHandler, movieHandler controller
 	}
 }
 
+func CORS(c *gin.Context) {
+
+	// First, we add the headers with need to enable CORS
+	// Make sure to adjust these headers to your needs
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Headers", "*")
+	c.Header("Content-Type", "application/json")
+
+	// Second, we handle the OPTIONS problem
+	if c.Request.Method != "OPTIONS" {
+
+		c.Next()
+
+	} else {
+
+		// Everytime we receive an OPTIONS request,
+		// we just return an HTTP 200 Status Code
+		// Like this, Angular can now do the real
+		// request using any other method than OPTIONS
+		c.AbortWithStatus(http.StatusOK)
+	}
+}
+
 func main() {
 	mongoURI := fmt.Sprintf(os.Getenv("MONGO_URI"))
 	app, err := InitialApplication(mongoURI, 1*time.Second)
@@ -35,7 +59,7 @@ func main() {
 	}
 
 	router := gin.Default()
-	router.Use(cors.Default())
+	router.Use(CORS)
 
 	userRoute := router.Group("/users")
 	routes.UserRoute(userRoute, app.UserHandler)
